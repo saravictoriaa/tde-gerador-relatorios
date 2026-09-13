a6c9991af829a7ffbecb57aaca555c15dde648ff
sara victoria <saravictoriadev@gmail.com>
2026-09-13 12:12:56 -0300

    refactor: cria model Relatorio.cs e contrato IGeradorRelatorio.cs

---
+++ Interfaces/IGeradorRelatorio..cs
@@ -0,0 +1,8 @@
+﻿using TdeGeradorRelatorios.Models;
+
+namespace TdeGeradorRelatorios.Interfaces;
+
+public interface IGeradorRelatorio
+{
+    void Gerar(Relatorio relatorio, string caminhoDestino);
+}
