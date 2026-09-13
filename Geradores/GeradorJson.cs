dd7792a227e3a17d78c9e169d73c160417225816
sara victoria <saravictoriadev@gmail.com>
2026-09-13 18:43:35 -0300

    refactor: extrai geradores de relatório

---
+++ Geradores/GeradorJson.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Text.Encodings.Web;
+using TdeGeradorRelatorios.Interfaces;
+using TdeGeradorRelatorios.Models;
+
+namespace TdeGeradorRelatorios.Geradores;
+
+public class GeradorJson : IGeradorRelatorio
+{
+    public void Gerar(Relatorio relatorio, string caminhoDestino)
+    {
+        var opcoes = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+
+        var dados = new
+        {
+            titulo = relatorio.Titulo,
+            responsavel = relatorio.Responsavel,
+            descricao = relatorio.Descricao,
+            data = relatorio.Data.ToString("dd/MM/yyyy HH:mm")
+        };
+
+        string conteudoJson = JsonSerializer.Serialize(dados, opcoes);
+
+        File.WriteAllText(caminhoDestino, conteudoJson);
+    }
+}
