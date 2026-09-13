8c921b731c4ff3c25751b57df178c0319928dba8
sara victoria <saravictoriadev@gmail.com>
2026-09-13 17:20:19 -0300

    refactor: extrai gerador de CSV

---
+++ Geradores/GeradorCsv.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using TdeGeradorRelatorios.Interfaces;
+using TdeGeradorRelatorios.Models;
+
+namespace TdeGeradorRelatorios.Geradores;
+
+public class GeradorCsv : IGeradorRelatorio
+{
+    public void Gerar(Relatorio relatorio, string caminhoDestino)
+    {
+        string conteudoCsv =
+            "Título;Responsável;Descrição;Data" + Environment.NewLine +
+            $"{EscaparCampo(relatorio.Titulo)};" +
+            $"{EscaparCampo(relatorio.Responsavel)};" +
+            $"{EscaparCampo(relatorio.Descricao)};" +
+            $"{EscaparCampo(relatorio.Data.ToString("dd/MM/yyyy HH:mm:ss"))}";
+
+        File.WriteAllText(
+            caminhoDestino,
+            conteudoCsv,
+            new UTF8Encoding(true)
+        );
+    }
+
+    private static string EscaparCampo(string valor)
+    {
+        return $"\"{valor.Replace("\"", "\"\"")}\"";
+    }
+}
